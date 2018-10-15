02f0481b3ed42d7c069e0772ead8853ca439f221
pauloptimizely <paul.serraino@optimizely.com>
2018-10-15 10:36:38 -0500

    Add redux

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 
 import './app.styl';
 
 import UniversalComponent from './components/UniversalComponent';
+
+class Test extends Component {
+    render() {
+        return (
+            <h1>Test: { this.props.data }</h1>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    data: state,
+});
+
+const mapDispatchToProps = (dispatch) => ({}); 
+
+const ConnectTest = connect(mapStateToProps, mapDispatchToProps)(Test);
+
+const reducer = (state, action) => state;
+
+const store = createStore(reducer, 'hello world')
 
 /**
  * The `App` component is the entry point for the react app.
@@ -14,14 +36,17 @@ import UniversalComponent from './components/UniversalComponent';
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Helmet>
-                    <title>App Component | React Universal</title>
-                </Helmet>
+            <Provider store={ store }>
+                <div>
+                    <Helmet>
+                        <title>App Component | React Universal</title>
+                    </Helmet>
 
-                <h1>Welcome to React Fiber.</h1>
-                <UniversalComponent name="getting-started" />
-            </div>
+                    <h1>Welcome to React Fiber.</h1>
+                    <UniversalComponent name="getting-started" />
+                    <ConnectTest />
+                </div>
+            </Provider>
         );
     }
 }
